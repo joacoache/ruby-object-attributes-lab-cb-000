511c2c6b053464e653b3a172e5091378fdd8ba36
Joaquín Herrera <herrerab.joaquin@gmail.com>
2018-05-15 19:56:09 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -3,7 +3,7 @@ class Dog
     @name = name_new
   end
 
-  def name(name)
+  def name
     @name
   end
 
@@ -11,7 +11,7 @@ class Dog
     @breed = breed_new
   end
 
-  def breed(breed)
+  def breed
     @breed
   end
 end
